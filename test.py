49746a3e998652185df31bfe4c3ee16e582b2328
Abel García <65343232+abelgarcia2@users.noreply.github.com>
2024-05-25 11:49:18 +0200

    delete legacy test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from bencoding.decoding import decode
-from bencoding.encoding import encode
-
-from  io import BytesIO
-
-with open('archlinux-2024.05.01-x86_64.iso.torrent', 'rb') as file:
-    decoded_data = decode(file)
